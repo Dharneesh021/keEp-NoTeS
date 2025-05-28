5948370dfaa9ac80d80d25e461837514f8e20d2c
Dharneesh J <155537423+Dharneesh021@users.noreply.github.com>
2025-05-28 20:31:08 +0530

    Update posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export default axios.create(
-   { baseURL : 'https://json-server-db-u875.onrender.com/' || process.env.REACT_APP_API_URL}
+   { baseURL : 'https://json-server-db-u875.onrender.com/posts' || process.env.REACT_APP_API_URL}
 )
