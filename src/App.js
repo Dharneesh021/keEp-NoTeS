1dd4ce3fdc0634892638d2af536b29206089b0b5
Dharneesh J <155537423+Dharneesh021@users.noreply.github.com>
2025-05-28 20:06:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   const [editBody , setEditBody] = useState('')
   const navigate = useNavigate();
   const {width} = useWindowSize();
-  const {data , isLoading , fetchErrors} = useAxiosFetch('http://localhost:4000/posts')
+  const {data , isLoading , fetchErrors} = useAxiosFetch('https://json-server-db-u875.onrender.com/' || process.env.REACT_APP_API_URL)
 
 
   // Fetch Data
