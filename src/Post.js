0206af61bd90f0045207cf4a53ee4c000eeea536
Dharneesh J <155537423+Dharneesh021@users.noreply.github.com>
2025-05-28 22:12:20 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Post = ({ elemets }) => {
-  const element = elemets;
+const Post = ({ element }) => {
   return (
     <article className="post">
-                <Link to={`post/${elements.id}`}>
-                      <h2>{elemets.title}</h2>
-                      <p className='postDate'>{elemets.datetime}</p>
-                </Link>
-                <p className='postBody'>{(elemets.body).length <= 25 ? elemets.body : `${(elemets.body).slice(0,25)}...`}</p>
-     </article>
-  )
+      <Link to={`post/${element.id}`}>
+        <h2>{element.title}</h2>
+        <p className='postDate'>{element.datetime}</p>
+      </Link>
+      <p className='postBody'>
+        {element.body.length <= 25 ? element.body : `${element.body.slice(0, 25)}...`}
+      </p>
+    </article>
+  );
 }
+
 
 export default Post
